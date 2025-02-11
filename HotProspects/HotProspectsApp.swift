67976d18b3ac514e5ab75f54bb317f4e5228237c
chiji1108 <shingo.pad@gmail.com>
2025-02-11 12:19:56 +0900

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by 千々岩真吾 on 2025/02/11.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
