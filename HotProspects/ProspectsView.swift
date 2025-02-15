79e3eb3e8679f893c85f5181d9003a86136a539e
chiji1108 <shingo.pad@gmail.com>
2025-02-15 17:32:44 +0900

    Enhance ProspectsView with dynamic filtering and list display

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -10,13 +10,26 @@ import SwiftUI
 
 struct ProspectsView: View {
     @Environment(\.modelContext) var modelContext
-    @Query(sort: \Prospect.name) var prospects: [Prospect]
+    @Query var prospects: [Prospect]
 
     enum FilterType {
         case none, contacted, uncontacted
     }
 
     let filter: FilterType
+
+    init(filter: FilterType) {
+        self.filter = filter
+
+        if filter != .none {
+            let showContactedOnly = filter == .contacted
+
+            _prospects = Query(
+                filter: #Predicate {
+                    $0.isContacted == showContactedOnly
+                }, sort: [SortDescriptor(\Prospect.name)])
+        }
+    }
 
     var title: String {
         switch filter {
@@ -31,16 +44,23 @@ struct ProspectsView: View {
 
     var body: some View {
         NavigationStack {
-            Text("People: \(prospects.count)")
-                .navigationTitle(title)
-                .toolbar {
-                    Button("Scan", systemImage: "qrcode.viewfinder") {
-                        let prospect = Prospect(
-                            name: "Paul Hudson", emailAddress: "paul@hackingwithswift.com",
-                            isContacted: false)
-                        modelContext.insert(prospect)
-                    }
+            List(prospects) { prospect in
+                VStack(alignment: .leading) {
+                    Text(prospect.name)
+                        .font(.headline)
+                    Text(prospect.emailAddress)
+                        .foregroundStyle(.secondary)
                 }
+            }
+            .navigationTitle(title)
+            .toolbar {
+                Button("Scan", systemImage: "qrcode.viewfinder") {
+                    let prospect = Prospect(
+                        name: "Paul Hudson", emailAddress: "paul@hackingwithswift.com",
+                        isContacted: false)
+                    modelContext.insert(prospect)
+                }
+            }
         }
     }
 }
